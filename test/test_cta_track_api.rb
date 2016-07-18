caf816470b084ace36e6da940c715f0ff28740f9
rdalin82 <Robert.Dalin@gmail.com>
2016-07-18 18:17:09 -0500

    added test to test type of what is returned in method

--- test/test_cta_track_api.rb
+++ test/test_cta_track_api.rb
@@ -3,13 +3,13 @@ require 'json'
 
 class CtaTrackAPI < Minitest::Test
   def setup
-    config = YAML.load(File.open(File.expand_path("../../config.yml", __FILE__), "r"))
+    config = YAML.load(File.open(File.expand_path("../../config.yml", __FILE__), "rb"))
     @key = config['API_KEY'] 
   end
 
   def test_it_will_have_an_api_key
     CtaTrack::API.apikey = "yo"
-    assert CtaTrack::API
+    assert_equal "yo", CtaTrack::API.apikey
   end
   def test_key_will_not_be_default
     refute_equal "YOUR API KEY HERE", CtaTrack::API.new.apikey
@@ -26,6 +26,16 @@ class CtaTrackAPI < Minitest::Test
     assert CtaTrack::API.routes()
     assert CtaTrack::API.routes(:routes=>78)
     assert CtaTrack::API.routes(:routes=>[78, 82])
-    puts CtaTrack::API.routes().class
+  end
+
+  def it_will_will_return_type_of_xml
+    CtaTrack::API.apikey = @key
+    result = Nokogiri::XML::Document
+    assert_equal result, CtaTrack::API.vehicles(:routes=>[78, 82]).class
+    assert_equal result, CtaTrack::API.vehicles(:routes=>82).class
+    assert_equal result, CtaTrack::API.vehicles(:routes=>92).class
+    assert_equal result, CtaTrack::API.vehicles(:routes=>[22]).class
+    assert_equal result, CtaTrack::API.routes(:routes=>78).class
+    assert_equal result, CtaTrack::API.routes(:routes=>[78, 82])
   end    
 end
